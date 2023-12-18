9eec352a3ed7f80f639c871e6c10beeaee250310
fer <rodriguezcortes.fer@gmail.com>
2023-12-18 13:16:41 +1300

    Fixed all packer related issues + added rose-pine complete theming

--- lua/perso/packer.lua
+++ lua/perso/packer.lua
@@ -1,8 +1,6 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -12,7 +10,21 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
 
+  -- Lua
+use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+end
+}
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use {
 	  'VonHeikemen/lsp-zero.nvim',
